d83152ed96a07af05d29067bed61d385e3ba2cf4
Maria <mashkay@gmail.com>
2022-09-19 23:07:35 +0300

    user+email+role

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+ROLES = (
+    ("user", "user"),
+    ("moderator", "moderator"),
+    ("admin", "admin"),
+)
 class User(AbstractUser):
+    
+    email = models.EmailField(
+        blank=False, max_length=254, verbose_name="Email"
+    )
     bio = models.TextField(
-        'Биография',
+        verbose_name="Биография",
         blank=True,
     )
+    role = models.CharField(choices=ROLES, max_length=20)
