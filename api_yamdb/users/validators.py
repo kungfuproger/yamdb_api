772dab300ebf10bd64232c40ff1dd1e63babd3a3
Maria <mashkay@gmail.com>
2022-09-20 19:40:17 +0300

    black

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -3,5 +3,5 @@ from django.core.exceptions import ValidationError
 
 def custom_username_validator(username):
     """Проверяет что username НЕ me"""
-    if username=="me":
+    if username == "me":
         raise ValidationError("username can't be <me>")
