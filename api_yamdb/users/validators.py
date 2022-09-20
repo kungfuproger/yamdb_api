9c3cbf10cdac1e3a6281f7f439a557238551788a
Maria <mashkay@gmail.com>
2022-09-20 09:52:00 +0300

    валидатор username!=me

---
+++ api_yamdb/users/validators.py
@@ -0,0 +1,7 @@
+from django.core.exceptions import ValidationError
+
+
+def custom_username_validator(username):
+    """Проверяет что username НЕ me"""
+    if username=="me":
+        raise ValidationError("username can't be <me>")
