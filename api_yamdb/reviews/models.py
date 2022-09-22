1b305f2faa7e6158c1e7fe4026361ebcecd778ba
Марк <abysiilator@gmail.com>
2022-09-22 09:11:46 +0300

    add_Meta_and_(__str__)_to_mark_models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -6,52 +6,62 @@ from users.models import User
 
 
 class Review(models.Model):
+    """Модель отзыва."""
+
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор',
+        related_name="reviews",
+        verbose_name="Автор",
     )
-#    title = models.ForeignKey(
-#        Title,
-#        on_delete=models.CASCADE,
-#        related_name='reviews',
-#        verbose_name='Произведение',
-#    )
+    #    title = models.ForeignKey(
+    #        Title,
+    #        on_delete=models.CASCADE,
+    #        related_name='reviews',
+    #        verbose_name='Произведение',
+    #    )
     text = models.TextField(
-        'Отзыв',
+        "Отзыв",
     )
     score = models.IntegerField(
-        'Оценка',
-        help_text='Поставьте оценку от 1 до 10',
-        validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)
-        ]
+        "Оценка",
+        help_text="Поставьте оценку от 1 до 10",
+        validators=[MaxValueValidator(10), MinValueValidator(1)],
     )
-    pub_date = models.DateTimeField(
-        'Дата публикации',
-        auto_now_add=True
-    )
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+        verbose_name = "Отзывы"
+
+    def __str__(self):
+        return f"Отзыв {self.author} на произведение {self.title}"
 
 
 class Comment(models.Model):
+    """Модель кооментария."""
+
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор',
+        related_name="comments",
+        verbose_name="Автор",
     )
     review = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='к этому отзыву',
+        related_name="comments",
+        verbose_name="к этому отзыву",
     )
     text = models.TextField(
-        'Комментарий',
+        "Комментарий",
     )
-    pub_date = models.DateTimeField(
-        'Дата публикации',
-        auto_now_add=True
-    )
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+        verbose_name = "Комментарии"
+
+    def __str__(self):
+        review = str(self.review)[6:]
+        return f"Комментарий {self.author} к отзыву {review}"
