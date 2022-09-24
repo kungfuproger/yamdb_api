707893931184c25df5753b776058f7fc9b03d751
Марк <abysiilator@gmail.com>
2022-09-25 00:44:51 +0300

    reviews_api/Mark

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,11 +4,11 @@ from rest_framework import permissions
 class AdminOrSuperuserOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.is_admin() or request.user.is_superuser)
+            request.user.is_admin() or request.user.is_superuser
+        )
 
 
-class Review(permissions.BasePermission):
-
+class SafeOrAuthorOrExceedingRoleOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
@@ -20,27 +20,8 @@ class Review(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
-            or request.user.is_superuser
-        )
-
-
-class Comment(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_user
-            or request.user.is_moderator
-            or request.user.is_admin
-            or request.user.is_superuser
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            obj.author == request.user
+            or obj.author == request.user
             or request.user.is_moderator
             or request.user.is_admin
             or request.user.is_superuser
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -14,8 +14,7 @@ from rest_framework_simplejwt.tokens import AccessToken
 
 from .permissions import (
     AdminOrSuperuserOnly,
-    ReviewPermissions,
-    CommentPermissions,
+    SafeOrAuthorOrExceedingRoleOnly
 )
 from .serializers import (
     AdminSerializer,
@@ -144,7 +143,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = (ReviewPermissions,)
+    permission_classes = (SafeOrAuthorOrExceedingRoleOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -154,7 +153,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (CommentPermissions,)
+    permission_classes = (SafeOrAuthorOrExceedingRoleOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
