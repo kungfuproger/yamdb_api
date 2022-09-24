f659d51c6c83dfd99d49813830f8619f015bd445
Tatiana Minenko <ta.minenko@ya.ru>
2022-09-24 19:35:25 +0300

    serializers + GenreTitle model

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from reviews.models import Category, Genre, GenreTitle, Title
 from users.models import User
 
 USER_FIELDS = (
@@ -49,3 +50,58 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = USER_FIELDS
         read_only_fields = ("role",)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ("name", "slug")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ("name", "slug")
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True, required=True)
+    category = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(), slug_field="slug", required=True
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            "id", "name", "year", "rating", "description", "genre", "category",
+        )
+
+    def create(self, validated_data):
+        genres = validated_data.pop("genre")
+        title = Title.objects.create(**validated_data)
+
+        for genre in genres:
+            current_genre, status = Genre.objects.get_or_create(**genre)
+            GenreTitle.objects.create(genre=current_genre, title=title)
+
+        return title
+
+    def update(self, instance, validated_data):
+        genres = validated_data.pop("genre")
+        instance.name = validated_data.get("name", instance.name)
+        instance.year = validated_data.get("year", instance.year)
+        instance.description = validated_data.get(
+            "description", instance.description
+        )
+        instance.category = validated_data.get("category", instance.category)
+
+        for genre in genres:
+            current_genre, status = Genre.objects.get_or_create(**genre)
+            GenreTitle.objects.get_or_create(
+                genre=current_genre, title=instance
+            )
+
+        instance.save()
+        return instance
