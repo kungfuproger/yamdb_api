93b59c19ceab9aa57f61aae05570c22342eb46c5
Марк <abysiilator@gmail.com>
2022-09-25 12:53:04 +0300

    rviews_api/Mark/minifix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -19,7 +19,7 @@ class SafeOrAuthorOrExceedingRoleOnly(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            or request.user.is_moderator
-            or request.user.is_admin
-            or request.user.is_superuser
+            or request.user.is_moderator()
+            or request.user.is_admin()
+            or request.user.is_superuser()
         )
