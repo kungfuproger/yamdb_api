bd2f14e39ebf079384f14590f1b5f27474a8f09e
Maria <mashkay@gmail.com>
2022-09-28 22:57:50 +0300

    auth endpoints  отдельным списком

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -21,10 +21,12 @@ router.register(
     CommentViewSet,
     basename="comment",
 )
-
+auth = [
+    path("token/", GetJWTokenView.as_view(), name="get_jwtoken"),
+    path("signup/", SignUpView.as_view(), name="sign_up"),
+]
 
 urlpatterns = [
     path("v1/", include(router.urls)),
-    path("v1/auth/token/", GetJWTokenView.as_view(), name="get_jwtoken"),
-    path("v1/auth/signup/", SignUpView.as_view(), name="sign_up"),
+    path("v1/auth/", include(auth)),
 ]
